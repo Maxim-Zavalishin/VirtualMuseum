971044714d810d6ef2e235fefd15ff45196d93de
Максим <ZavalishinMaxim@mail.ru>
2024-05-03 18:59:46 +0400

    Добавление зависимостей для слоя Aplication. Настройка авторизации и аутентификации. Настройка Swagger.

---
+++ VirtualMuseum.Api/Startup.cs
@@ -0,0 +1,69 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using VirtualMuseum.Domain.Settings;
+
+namespace VirtualMuseum.Api;
+
+public static class Startup
+{
+    public static void AddSwagger(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                In = ParameterLocation.Header,
+                Description = "Введите валидный токен",
+                Name = "Авторизация",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme()
+                    {
+                        Reference = new OpenApiReference()
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Name = "Bearer",
+                        In = ParameterLocation.Header
+                    }, Array.Empty<string>()
+                }
+            });
+        });
+    }
+    
+    public static void AddAuthenticationAndAuthorization(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        services.AddAuthorization();
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(o =>
+            {
+                var options = builder.Configuration.GetSection(JwtSettings.DefaultSections).Get<JwtSettings>();
+
+                o.Authority = options.Authority;
+                o.RequireHttpsMetadata = false;
+                o.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidIssuer = options.Issuer,
+                    ValidAudience = options.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(options.JwtKey)),
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true
+                };
+            });
+    }
+}
---
+++ VirtualMuseum.Application/DependensyInjection/DependensyInjection.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VirtualMuseum.Application.AutoMapper.UserMapper;
+using VirtualMuseum.Application.Services;
+using VirtualMuseum.Domain.Interfaces.Services;
+
+namespace VirtualMuseum.Application.DependensyInjection;
+
+public static class DependensyInjection
+{
+    public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMappers();
+        services.InitServices();
+    }
+    private static void AddMappers(this IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(UserMapper));
+        services.AddAutoMapper(typeof(UserRegisterMapper));
+    }
+
+    private static void InitServices(this IServiceCollection services)
+    {
+        services.AddTransient<IAuthServices, AuthService>();
+        services.AddTransient<ITokenServices, TokenService>();
+    }
+}
